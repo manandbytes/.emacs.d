db7f423598c87849b0d8ea9a08b52c31aa21c296
Mykola Nikishov <mn@mn.com.ua>
2014-01-04 03:08:48 +0200

    Use local proxy for HTTP requests

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,6 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(backup-directory-alist (quote (("." . "~/.emacs.d/backups/"))))
+ '(url-proxy-services
+   (quote
+    (("no_proxy" . "\\(localhost\\|127\\.0\\.0\\.0/8\\|::1\\)")
+     ("http" . "127.0.0.1:3128"))))
  '(vc-make-backup-files t)
  )
 (custom-set-faces
