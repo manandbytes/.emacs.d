50a4fc4c960af0e9c7bd81ec37f9a66405ae7aa4
Mykola Nikishov <mn@mn.com.ua>
2014-01-04 03:02:44 +0200

    Backup versioned files too
    I don’t commit on every save so it makes sense to back up
    intermediate copies too.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(backup-directory-alist (quote (("." . "~/.emacs.d/backups/")))))
+ '(backup-directory-alist (quote (("." . "~/.emacs.d/backups/"))))
+ '(vc-make-backup-files t)
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
