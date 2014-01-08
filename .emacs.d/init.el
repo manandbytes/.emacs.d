1366431fc5190ec6c0c4cc0b1ca4d9ccc875d062
Mykola Nikishov <mn@mn.com.ua>
2014-01-08 04:09:01 +0200

    Use single character response for 'yes or no' confirmations

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,8 @@
 (server-start)
+
+; yes-or-no -> y-or-n
+(fset 'yes-or-no-p 'y-or-n-p)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
