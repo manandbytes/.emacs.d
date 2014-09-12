3f3d8ee418c968ae585907e0aa22f8a4fb77fa13
Mykola Nikishov <mn@mn.com.ua>
2014-09-12 20:44:15 +0300

    org: strike out completed tasks

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -234,4 +234,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-done ((t (:strike-through t))))
+ '(org-headline-done ((t (:strike-through t))))
  )
